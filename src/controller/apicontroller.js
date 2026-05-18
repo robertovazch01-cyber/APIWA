ec7ef97ca31ef6ec61c385fc404bf39dfe82d6f7
Roberto Vazquez <mac@MACs-MacBook-Pro.local>
2026-05-17 23:30:24 -0600

    actualizacion rutas

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -5,7 +5,7 @@ const verificar = (req, res) => {
         var token = req.query ["hub.verify_token"];
         var challenge = req.query ["hub.challenge"];
 
-        if (challenge != null && token != null && token === tokenRV) {
+        if (challenge != null && token != null && token == tokenRV) {
             re.send (challenge);
         } else {
             res.status(400).send();
