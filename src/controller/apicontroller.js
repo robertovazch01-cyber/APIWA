f07bf3530f58bf47e3d5b29e59dcb1c2a8b5c842
Roberto Vazquez <mac@MACs-MacBook-Pro.local>
2026-05-17 22:53:39 -0600

    actualizacion rutas

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,9 +1,11 @@
 const verificar = (req, res) => {
     res.send('Verificado');
+    console.log('Verificado en consola');
 }
 
 const recibir = (req, res) => {
     res.send('Recibido');
+    console.log('Recibido en consola');
 }
 
 module.exports = {
